06c844e411e51155a847b67c22d1e48eb0ee8d29
张槿 <1748532092@qq.com>
2021-04-22 23:31:03 +0800

    新建积分详情页

---
+++ src/pages/more_pages/points_details.vue
@@ -0,0 +1,17 @@
+<template>
+    <view class="points_details">
+        <view class="header">
+
+        </view>
+    </view>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="sass" scoped>
+    // .points_details{
+    
+</style>
